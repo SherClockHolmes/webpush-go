4656e63a4d525f563e0605114d235d5e92b73131
Daniel Richter <Nexyz9@gmail.com>
2024-12-31 16:34:31 -0800

    Concatenate strings directly instead of using fmt (#46)
    This reduces unnecessary allocations
    
    Co-authored-by: Daniel Richter <drichter@undergroundelephant.com>
    Co-authored-by: Ethan Holmes <sherclockholmes@gmail.com>

--- vapid.go
+++ vapid.go
@@ -5,7 +5,6 @@ import (
 	"crypto/elliptic"
 	"crypto/rand"
 	"encoding/base64"
-	"fmt"
 	"math/big"
 	"net/url"
 	"time"
@@ -73,9 +72,9 @@ func getVAPIDAuthorizationHeader(
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
-		"aud": fmt.Sprintf("%s://%s", subURL.Scheme, subURL.Host),
-		"exp": expiration.Unix(),
-		"sub": fmt.Sprintf("mailto:%s", subscriber),
+		"aud": subURL.Scheme + "://" + subURL.Host,
+		"exp": time.Now().Add(time.Hour * 12).Unix(),
+		"sub": "mailto:" + subscriber,
 	})
 
 	// Decode the VAPID private key
@@ -98,11 +97,7 @@ func getVAPIDAuthorizationHeader(
 		return "", err
 	}
 
-	return fmt.Sprintf(
-		"vapid t=%s, k=%s",
-		jwtString,
-		base64.RawURLEncoding.EncodeToString(pubKey),
-	), nil
+	return "vapid t=" + jwtString + ", k=" + base64.RawURLEncoding.EncodeToString(pubKey), nil
 }
 
 // Need to decode the vapid private key in multiple base64 formats
